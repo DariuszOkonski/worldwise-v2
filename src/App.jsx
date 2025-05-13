83a20658446263552815af3f4d06e8e3e1517b2a
Dariusz Okoński <darek200180@gmail.com>
2025-05-13 14:54:54 +0200

    add CityItem

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,32 @@ import Product from './pages/Product';
 import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
+import { useEffect, useState } from 'react';
+
+const URL = `http://localhost:8000`;
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // TODO: move this to other place
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert('There was an error loading data...');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +40,14 @@ function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path='cities' element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path='cities'
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path='countries' element={<p>COUNTRIES</p>} />
           <Route path='form' element={<p>FORM</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable react/prop-types */
+import CityItem from './CityItem';
 import styles from './CityList.module.css';
+import Spinner from './Spinner';
 
-function CityList() {
+function CityList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  console.log(cities);
+
   return (
     <ul className={styles.cityList}>
-      <li>List</li>
-      <li>List</li>
-      <li>List</li>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
     </ul>
   );
 }

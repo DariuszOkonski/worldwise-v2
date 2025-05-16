81528f766f8e095310fda7b763a9b28015291a98
Dariusz Okoński <darek200180@gmail.com>
2025-05-16 10:00:07 +0200

    remove comments

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 import { createContext, useContext, useEffect, useState } from 'react';
 
@@ -9,7 +10,6 @@ function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  // TODO: move this to other place
   useEffect(() => {
     async function fetchCities() {
       try {

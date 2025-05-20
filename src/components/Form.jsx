e62c30771e8349dbb8a7b83fe4b6e23171b14c1e
Dariusz Okoński <darek200180@gmail.com>
2025-05-20 09:46:56 +0200

    add city to map

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,6 +9,7 @@ import Message from './Message';
 import Spinner from './Spinner';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useCities } from '../contexts/CitiesContext';
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -22,6 +23,7 @@ const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
 
 function Form() {
   const [lat, lng] = useUrlPosition();
+  const { createCity } = useCities();
 
   const [cityName, setCityName] = useState('');
   const [country, setCountry] = useState('');
@@ -64,6 +66,21 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!cityName || !date) {
+      return;
+    }
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+
+    createCity(newCity);
   }
 
   if (isLoadingGeocoding) {
